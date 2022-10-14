14bcef71a1343075d2c620241ad4139f05416af4
Chaoran Liu <chaoran.liu@icloud.com>
2022-10-14 15:41:15 +0800

    Create recover.sh

---
+++ recover.sh
@@ -0,0 +1,53 @@
+# Pi Hole
+echo installing pi-hole
+curl -sSL https://install.pi-hole.net | bash
+
+# lighttpd
+echo enable lighttpd dir-listing
+echo 'server.dir-listing = "enable"' | sudo tee -a /etc/lighttpd/lighttpd.conf
+sudo systemctl restart lighttpd.service
+
+# aria
+echo installing aria2
+sudo apt update
+sudo apt install -y aria2
+
+echo "continue
+dir=/var/www/downloads
+file-allocation=falloc
+max-connection-per-server=4
+max-concurrent-downloads=2
+max-overall-download-limit=0
+min-split-size=25M
+rpc-allow-origin-all=true
+rpc-secret=100200
+input-file=/var/tmp/aria2c.session
+save-session=/var/tmp/aria2c.session" > aria2.daemon
+sudo mv aria2.daemon /etc
+
+echo "# Override or Change User and Group per your local environment
+[Unit]
+Description=Aria2c download manager
+After=network.target
+
+[Service]
+Type=simple
+User=www-data
+Group=www-data
+ExecStartPre=/usr/bin/env touch /var/tmp/aria2c.session
+ExecStart=/usr/bin/aria2c --console-log-level=warn --enable-rpc --rpc-listen-all --conf-path=/etc/aria2.daemon
+TimeoutStopSec=20
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > aria2c.service
+sudo mv aria2c.service /etc/systemd/system
+sudo systemctl daemon-reload
+
+# ariang - aria UI
+wget https://github.com/mayswind/AriaNg/releases/download/1.2.4/AriaNg-1.2.4-AllInOne.zip
+sudo mkdir --parents /var/www/html/ariang
+sudo unzip AriaNg-1.2.4-AllInOne.zip -d /var/www/html/ariang
+rm AriaNg-1.2.4-AllInOne.zip 
+
+sudo systemctl start aria2c.service
